bee67e87ec4281dbebfe4c39c0917c6e0c22507d
Feifei Sun <feifeisun0503@gmail.com>
2023-08-29 15:18:14 -0400

    Setup Create Thread page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,10 +2,19 @@
 
 import { FilterQuery, SortOrder } from "mongoose";
 import { revalidatePath } from "next/cache";
-
 import User from "../models/user.model";
 
 import { connectToDB } from "../mongoose";
+
+export async function fetchUser(userId: string) {
+  try {
+    connectToDB();
+
+    return await User.findOne({ id: userId });
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user: ${error.message}`);
+  }
+}
 
 interface Params {
   userId: string;
