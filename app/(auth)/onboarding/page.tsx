2f8d042c71c5963fca630541d59ba131830c5117
Feifei Sun <feifeisun0503@gmail.com>
2023-08-30 22:40:18 -0400

    Fix onboarding redirect issue

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,22 +1,25 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+import { fetchUser } from "@/lib/actions/user.actions";
 import AccountProfile from "@/components/forms/AccountProfile";
 
 async function Page() {
-    const user = await currentUser();
-    if (!user) return null; // to avoid typescript warnings
-  
-    const userInfo = {};
-  
-    const userData = {
-      id: user.id,
-      objectId: userInfo?._id,
-      username: userInfo ? userInfo?.username : user.username,
-      name: userInfo ? userInfo?.name : user.firstName ?? "",
-      bio: userInfo ? userInfo?.bio : "",
-      image: userInfo ? userInfo?.image : user.imageUrl,
-    };
+  const user = await currentUser();
+  if (!user) return null; // to avoid typescript warnings
+
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
+
+  const userData = {
+    id: user.id,
+    objectId: userInfo?._id,
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user.imageUrl,
+  };
+
   return (
     <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
       <h1 className='head-text'>Onboarding</h1>
