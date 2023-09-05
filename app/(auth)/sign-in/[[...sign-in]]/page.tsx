4fb98a6203337a752d783df1df78919de8c8d043
Feifei Sun <feifeisun0503@gmail.com>
2023-09-05 17:41:34 -0400

    Fix sign-up page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,27 @@
 import { SignIn } from "@clerk/nextjs";
  
 export default function Page() {
-  return <SignIn />;
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <SignIn />
+
+        <section className='px-8 py-8 text-white'>
+          <h1 className="text-lg font-bold">
+            Secure sign-up powered by Clerk.
+          </h1>
+          <h1 className="text-lg font-bold">
+            If you don't wish to sign up,
+          </h1>
+          <h1 className="text-lg font-bold">
+          feel free to explore content using this account:
+          </h1>
+          <p>
+            Email: <span className="font-mono text-yellow-400">threads.test.123@gmail.com</span>
+          </p>
+          <p>Password: <span className="font-mono text-yellow-400">threadstest</span></p>
+        </section>
+      </div>
+    </>
+  );
 }
